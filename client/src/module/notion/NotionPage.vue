a5528e0b5b7c1f9853350be05a5d5239de4dc47f
huybn5776 <aseeme555076@gmail.com>
2024-02-04 18:32:10 +0800

    feat: handle Notion auth error

--- client/src/module/notion/NotionPage.vue
+++ client/src/module/notion/NotionPage.vue
@@ -7,6 +7,9 @@
 <script lang="ts" setup>
 import { onMounted } from 'vue';
 
+import type { OauthTokenResponse } from '@notionhq/client/build/src/api-endpoints';
+import { AxiosError } from 'axios';
+import { useNotification } from 'naive-ui';
 import { useRoute, useRouter } from 'vue-router';
 
 import { getNotionTokenByCode } from '@/api/notion-auth-api.service';
@@ -17,25 +20,43 @@ const oauthClientId = import.meta.env.VITE_NOTION_CLIENT_ID;
 const notionAuthUrl = `https://api.notion.com/v1/oauth/authorize?client_id=${oauthClientId}&response_type=code&owner=user`;
 const route = useRoute();
 const router = useRouter();
-
-async function getNotionToken(): Promise<string | null> {
-  const { code } = route.query;
-  if (typeof code !== 'string') {
-    return null;
-  }
-
-  const notionAuth = await getNotionTokenByCode(code);
-  const notionToken = notionAuth.access_token;
-  saveSettingToStorage(SettingKey.NotionAuth, notionAuth);
-
-  await router.push('notion');
-
-  return notionToken;
-}
+const notification = useNotification();
 
 onMounted(async () => {
   await getNotionToken();
 });
+
+async function getNotionToken(): Promise<void> {
+  const { code } = route.query;
+  if (typeof code !== 'string') {
+    return;
+  }
+  notification.destroyAll();
+
+  let notionAuth: OauthTokenResponse;
+  try {
+    notionAuth = await getNotionTokenByCode(code);
+  } catch (e) {
+    handleAuthError(e);
+    return;
+  }
+
+  saveSettingToStorage(SettingKey.NotionAuth, notionAuth);
+  await router.push('notion');
+}
+
+function handleAuthError(e: unknown): void {
+  console.error(e);
+
+  const error = e as AxiosError;
+  const responseData = error.response?.data as { error: string; message: string } | undefined;
+  notification.error({
+    title: `Fail to integrate with Notion`,
+    content: responseData?.message,
+    duration: 0,
+    closable: true,
+  });
+}
 </script>
 
 <style lang="scss" scoped>
