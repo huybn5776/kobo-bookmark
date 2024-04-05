47eb84633c2a97c3bcd323a4c1c6d5908cb83a00
huybn5776 <aseeme555076@gmail.com>
2024-04-05 15:52:12 +0800

    feat: click cover image to view

--- client/src/module/bookmarks/component/BookCoverView/BookCoverView.vue
+++ client/src/module/bookmarks/component/BookCoverView/BookCoverView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="book-cover-view">
-    <img v-if="book.coverImageUrl" class="book-cover-image" :src="book.coverImageUrl" :alt="book.info.title" />
+    <NImage v-if="book.coverImageUrl" class="book-cover-image" :src="book.coverImageUrl" :alt="book.info.title" />
     <div v-if="!book.coverImageUrl" class="fallback-book-cover">
       <span class="fallback-image-cover-text">
         {{ book.info.title }}
@@ -10,6 +10,8 @@
 </template>
 
 <script lang="ts" setup>
+import { NImage } from 'naive-ui';
+
 import { KoboBook } from '@/dto/kobo-book';
 
 defineProps<{ book: KoboBook }>();
