77ddcbb20f622cfd67f7feb58d3bc3c56fbd624b
huybn5776 <aseeme555076@gmail.com>
2024-05-26 14:55:57 +0800

    refactor: extract text part of chapter view as a standalone component

--- client/src/module/bookmarks/component/BookmarkChapterView/BookmarkChapterView.vue
+++ client/src/module/bookmarks/component/BookmarkChapterView/BookmarkChapterView.vue
@@ -1,32 +1,14 @@
 <template>
   <p class="bookmark-chapter-view" :class="{ 'bookmark-chapter-archived': archived }">
-    <span v-for="parentChapter in parentChapters" :key="parentChapter.index" class="bookmark-parent-chapter-title">
-      {{ parentChapter.title }}
-    </span>
-    <span v-for="title in titles" :key="title" class="bookmark-chapter-title">{{ title }}</span>
+    <BookmarkChapterViewContent :chapterIndexMap="chapterIndexMap" :chapter="chapter" />
   </p>
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
-
-import { prop } from 'ramda';
-
 import { KoboBookmarkChapter, KoboBookChapter } from '@/dto/kobo-book';
+import BookmarkChapterViewContent from '@/module/bookmarks/component/BookmarkChapterViewContent/BookmarkChapterViewContent.vue';
 
-const props = defineProps<{
-  chapterIndexMap: Record<number, KoboBookChapter>;
-  chapter: KoboBookmarkChapter;
-  archived?: boolean;
-}>();
-
-const parentChapters = computed<KoboBookChapter[]>(() => {
-  return (props.chapter.parentChapterIndexes || []).map((index) => props.chapterIndexMap[index]);
-});
-const titles = computed(() => {
-  const relatedChapters = props.chapter.relatedChapterIndexes.map((index) => props.chapterIndexMap[index]);
-  return relatedChapters.map(prop('title'));
-});
+defineProps<{ chapterIndexMap: Record<number, KoboBookChapter>; chapter: KoboBookmarkChapter; archived?: boolean }>();
 </script>
 
 <style lang="scss" scoped>
---
+++ client/src/module/bookmarks/component/BookmarkChapterViewContent/BookmarkChapterViewContent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bookmark-chapter-view-content">
+    <span v-for="parentChapter in parentChapters" :key="parentChapter.index" class="bookmark-parent-chapter-title">
+      {{ parentChapter.title }}
+    </span>
+    <span v-for="title in titles" :key="title" class="bookmark-chapter-title">{{ title }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+import { prop } from 'ramda';
+
+import { KoboBookmarkChapter, KoboBookChapter } from '@/dto/kobo-book';
+
+const props = defineProps<{ chapterIndexMap: Record<number, KoboBookChapter>; chapter: KoboBookmarkChapter }>();
+
+const parentChapters = computed<KoboBookChapter[]>(() => {
+  return (props.chapter.parentChapterIndexes || []).map((index) => props.chapterIndexMap[index]);
+});
+const titles = computed(() => {
+  const relatedChapters = props.chapter.relatedChapterIndexes.map((index) => props.chapterIndexMap[index]);
+  return relatedChapters.map(prop('title'));
+});
+</script>
+
+<style lang="scss" scoped>
+@import './BookmarkChapterViewContent';
+</style>
